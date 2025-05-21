4d1f77fec75a7c33931a81a29c7ea359ceda44f5
Chanchlesh <Chanchlesh.dev@gmail.com>
2025-05-21 23:57:09 +0530

    Home screen intial setup added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import { Moon, Sun } from "lucide-react";
 import "./App.css";
+import Home from "./components/crypto-ui/Home";
 import Navbar from "./components/crypto-ui/Navbar";
-import { Button } from "./components/ui/button";
-import { useTheme } from "./hooks/useTheme";
 
 function App() {
-  const { theme, toggleTheme } = useTheme();
+  // const { theme, toggleTheme } = useTheme();
 
   return (
     <>
       <Navbar />
-      <Button
+      <Home />
+      {/* <Button
         variant="outline"
         size="icon"
         onClick={toggleTheme}
         title={`Switch to ${theme === "light" ? "dark" : "light"} theme`}
       >
         {theme === "light" ? <Moon /> : <Sun />}
-      </Button>
+      </Button> */}
     </>
   );
 }
--- src/components/crypto-ui/Navbar.tsx
+++ src/components/crypto-ui/Navbar.tsx
@@ -1,7 +1,8 @@
-import { Avatar } from "@radix-ui/react-avatar";
-import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { useTheme } from "@/hooks/useTheme";
+import { HamburgerMenuIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import icon from "../../assets/polygon.png";
-import { AvatarImage } from "../ui/avatar";
+import profileIcon from "../../assets/profile.png";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import {
   Sheet,
@@ -12,11 +13,13 @@ import {
   SheetTrigger,
 } from "../ui/sheet";
 import Sidebar from "./Sidebar";
+import { Moon, Sun } from "lucide-react";
 
 const Navbar = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="px-2 py-3 border-b z-50 bg-background bg-opacity-0 sticky top-0 left-0 right-0 flex justify-between items-center">
-      <div className="flex items-center gap-2">
+    <div className="px-2 py-3 border-b z-50 bg-background sticky top-0 left-0 right-0 flex justify-between items-center">
+      <div className="flex items-center gap-2 ">
         <Sheet>
           <SheetTrigger>
             <Button
@@ -52,12 +55,49 @@ const Navbar = () => {
                   </div>
                 </div>
               </SheetTitle>
-              <SheetDescription>
-                <Sidebar />
-              </SheetDescription>
+              <SheetDescription></SheetDescription>
+              <Sidebar />
             </SheetHeader>
           </SheetContent>
         </Sheet>
+        <p className="text-sm lg:text-base cursor-pointer ">
+          <span className="text-2xl font-bold text-orange-700">Crypto</span>
+          <span className="text-2xl font-bold text-gray-500">Wave</span>
+        </p>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <div className="p-0 ml-2 ">
+          <Button
+            variant="outline"
+            className="flex items-center gap-2 rounded-full w-35"
+          >
+            <MagnifyingGlassIcon className="h-10 w-10" />
+            <span className="text-sm font-semibold">Search</span>
+          </Button>
+        </div>
+        <div>
+          <Button
+            variant="outline"
+            size="icon"
+            className="rounded-full h-10 w-10"
+            onClick={toggleTheme}
+            title={`Switch to ${theme === "light" ? "dark" : "light"} theme`}
+          >
+            {theme === "light" ? <Moon /> : <Sun />}
+          </Button>
+        </div>
+
+        <div>
+          <Avatar className="h-10 w-10">
+            <AvatarFallback>CW</AvatarFallback>
+            <AvatarImage
+              src={profileIcon}
+              alt="Coin icon"
+              className="rounded-full"
+            />
+          </Avatar>
+        </div>
       </div>
     </div>
   );
